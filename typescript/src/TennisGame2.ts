64686ca1fe12faa4d1743445c8a196540aa1f187
Colin DeCarlo <c.decarlo@vehikl.com>
2019-05-15 10:29:37 -0400

    Cleaned up return

--- typescript/src/TennisGame2.ts
+++ typescript/src/TennisGame2.ts
@@ -43,16 +43,24 @@ export class TennisGame2 implements TennisGame {
   }
 
   private playerTwoLeading(score: string) {
-    if (this.P2point > this.P1point && this.P2point < 4) {
-      if (this.P2point === 2)
-        this.P2res = 'Thirty';
-      if (this.P2point === 3)
-        this.P2res = 'Forty';
-      if (this.P1point === 1)
-        this.P1res = 'Fifteen';
-      if (this.P1point === 2)
-        this.P1res = 'Thirty';
-      score = this.P1res + '-' + this.P2res;
+    const player2points = this.P2point;
+    const player1points = this.P1point;
+    let player1res = this.P1res;
+    let player2res = this.P2res;
+    return this.pointsToWords(player2points, player1points, player2res, player1res, score);
+  }
+
+  private pointsToWords(player2points: number, player1points: number, player2res: string, player1res: string, score: string) {
+    if (player2points > player1points && player2points < 4) {
+      if (player2points === 2)
+        player2res = 'Thirty';
+      if (player2points === 3)
+        player2res = 'Forty';
+      if (player1points === 1)
+        player1res = 'Fifteen';
+      if (player1points === 2)
+        player1res = 'Thirty';
+      score = player1res + '-' + player2res;
     }
     return score;
   }
