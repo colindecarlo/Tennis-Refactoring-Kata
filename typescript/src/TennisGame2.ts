b39c2b9d9dd6f03822aa68c81ff29972906003e8
Colin DeCarlo <c.decarlo@vehikl.com>
2019-05-15 10:14:52 -0400

    extracted method

--- typescript/src/TennisGame2.ts
+++ typescript/src/TennisGame2.ts
@@ -15,28 +15,8 @@ export class TennisGame2 implements TennisGame {
     score = this.playerOneAhead(score);
     score = this.playerTwoAhead(score);
 
-    if (this.P1point > this.P2point && this.P1point < 4) {
-      if (this.P1point === 2)
-        this.P1res = 'Thirty';
-      if (this.P1point === 3)
-        this.P1res = 'Forty';
-      if (this.P2point === 1)
-        this.P2res = 'Fifteen';
-      if (this.P2point === 2)
-        this.P2res = 'Thirty';
-      score = this.P1res + '-' + this.P2res;
-    }
-    if (this.P2point > this.P1point && this.P2point < 4) {
-      if (this.P2point === 2)
-        this.P2res = 'Thirty';
-      if (this.P2point === 3)
-        this.P2res = 'Forty';
-      if (this.P1point === 1)
-        this.P1res = 'Fifteen';
-      if (this.P1point === 2)
-        this.P1res = 'Thirty';
-      score = this.P1res + '-' + this.P2res;
-    }
+    score = this.playerOneLeading(score);
+    score = this.playerTwoLeading(score);
 
     if (this.P1point > this.P2point && this.P2point >= 3) {
       score = 'Advantage player1';
@@ -51,6 +31,36 @@ export class TennisGame2 implements TennisGame {
     }
     if (this.P2point >= 4 && this.P1point >= 0 && (this.P2point - this.P1point) >= 2) {
       score = 'Win for player2';
+    }
+    return score;
+  }
+
+  private playerTwoLeading(score: string) {
+    if (this.P2point > this.P1point && this.P2point < 4) {
+      if (this.P2point === 2)
+        this.P2res = 'Thirty';
+      if (this.P2point === 3)
+        this.P2res = 'Forty';
+      if (this.P1point === 1)
+        this.P1res = 'Fifteen';
+      if (this.P1point === 2)
+        this.P1res = 'Thirty';
+      score = this.P1res + '-' + this.P2res;
+    }
+    return score;
+  }
+
+  private playerOneLeading(score: string) {
+    if (this.P1point > this.P2point && this.P1point < 4) {
+      if (this.P1point === 2)
+        this.P1res = 'Thirty';
+      if (this.P1point === 3)
+        this.P1res = 'Forty';
+      if (this.P2point === 1)
+        this.P2res = 'Fifteen';
+      if (this.P2point === 2)
+        this.P2res = 'Thirty';
+      score = this.P1res + '-' + this.P2res;
     }
     return score;
   }
